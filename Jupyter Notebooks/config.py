a12678e4947685ff2c7470da4c98a40965c2d22b
cemoga <cemoga@yahoo.com>
2019-11-04 19:08:04 -0500

    Create config.py

---
+++ Jupyter Notebooks/config.py
@@ -0,0 +1,14 @@
+# Connection Data
+# Sample Connection Data
+# ServerName = "localhost"
+# UserName = "postgres"
+# Password = "postgres"
+# port = "5432"
+# DataBase = "ETL_db"
+
+
+ServerName = "localhost"
+UserName = "postgres"
+Password = "postgres"
+port = "5432"
+DataBase = "ETL_db"
